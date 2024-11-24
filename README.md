d501f164c9747a5452fb2edc4fe527909d1c9f09
robertarnorsson <rha006@gmail.com>
2024-11-24 10:20:00 +0100

    Stalker 2 zone tile server

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# stalker2zone-tiles
+# Tile Server API Documentation
+
+This is the Tile Server API for `stalker2.zone`. This API serves map tiles based on coordinates and provides access to tile data stored. This API is not public to use.
---
+++ src/index.ts
@@ -0,0 +1,51 @@
+import { Hono } from 'hono'
+
+export type Env = {
+  BUCKET: R2Bucket;
+}
+
+const app = new Hono<{ Bindings: Env }>()
+
+app.get('/', (c) => {
+  return c.text('Tile server for stalker2.zone')
+})
+
+app.get(':z/:x/:y', async (c) => {
+  const { BUCKET } = c.env;
+  const { z, x, y } = c.req.param();
+
+  if (isNaN(Number(z)) || isNaN(Number(x)) || isNaN(Number(y))) {
+    return c.text('Invalid tile coordinates', 400);
+  }
+
+  try {
+    const tilePath = `${z}/${x}/${y}.jpg`;
+
+    const tile = await BUCKET.get(tilePath);
+
+    if (!tile) {
+      return c.text('Tile not found', 404);
+    }
+
+    if (!tile.body) {
+      return c.text('Tile content is empty', 500);
+    }
+
+    return c.body(tile.body, 200, {
+      'Content-Type': 'image/jpeg',
+      'Cache-Control': 'public, max-age=31536000'
+    });
+  } catch (error) {
+    console.error('Error fetching tile.');
+    
+    if (error instanceof TypeError) {
+      return c.text('Invalid tile format or bucket configuration', 500);
+    } else if (error instanceof RangeError) {
+      return c.text('Tile coordinates are out of range', 400);
+    }
+    
+    return c.text('Internal Server Error', 500);
+  }
+});
+
+export default app
